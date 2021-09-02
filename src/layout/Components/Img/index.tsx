e1a6f103b9387ed98132992c4a27561a5bc1bb42
blurname <naughtybao@outlook.com>
2021-09-02 13:01:48 +0800

    fix: move picture to public folder

--- src/layout/Components/Img/index.tsx
+++ src/layout/Components/Img/index.tsx
@@ -10,32 +10,27 @@ const imgs: ImgType[] = [
   {
     id: 1,
     style: { width: 80, height: 140 },
-    value:
-      'https://gss0.baidu.com/70cFfyinKgQFm2e88IuM_a/baike/pic/item/c995d143ad4bd1139851712355afa40f4bfb0507.jpg',
+    value: '../../../../public/test.jpg',
   },
   {
     id: 2,
     style: { width: 20, height: 190 },
-    value:
-      'https://gss0.baidu.com/70cFfyinKgQFm2e88IuM_a/baike/pic/item/c995d143ad4bd1139851712355afa40f4bfb0507.jpg',
+    value: '../../../../public/test.jpg',
   },
   {
     id: 3,
     style: { width: 30, height: 180 },
-    value:
-      'https://gss0.baidu.com/70cFfyinKgQFm2e88IuM_a/baike/pic/item/c995d143ad4bd1139851712355afa40f4bfb0507.jpg',
+    value: '../../../../public/test.jpg',
   },
   {
     id: 4,
     style: { width: 40, height: 180 },
-    value:
-      'https://gss0.baidu.com/70cFfyinKgQFm2e88IuM_a/baike/pic/item/c995d143ad4bd1139851712355afa40f4bfb0507.jpg',
+    value: '../../../../public/test.jpg',
   },
   {
     id: 5,
     style: { width: 50, height: 190 },
-    value:
-      'https://gss0.baidu.com/70cFfyinKgQFm2e88IuM_a/baike/pic/item/c995d143ad4bd1139851712355afa40f4bfb0507.jpg',
+    value: '../../../../public/test.jpg',
   },
 ]
 
