45eff90b9e8b00649554db8c5149acd79de8979d
blurname <naughtybao@outlook.com>
2021-11-17 19:00:00 +0800

    feat: a new maze mosaic

--- src/filter/shader.ts
+++ src/filter/shader.ts
@@ -690,3 +690,60 @@ export const MosaicSnowShader = {
     layer: { type: float },
   },
 }
+
+const mosaicMazeFS = `
+
+precision mediump float;
+
+#define PI 3.14159265358979323846
+
+varying vec2 vTexCoord;
+uniform vec2 u_resolution;
+
+float random (in vec2 _st) {
+    return fract(sin(dot(_st.xy,
+                        vec2(12.9898,78.233)))*
+        43758.5453123);
+}
+
+vec2 truchetPattern(in vec2 _st, in float _index){
+    _index = fract(((_index-0.5)*2.0));
+    if (_index > 0.75) {
+        _st = vec2(1.0) - _st;
+    } else if (_index > 0.5) {
+        _st = vec2(1.0-_st.x,_st.y);
+    } else if (_index > 0.25) {
+        _st = 1.0-vec2(1.0-_st.x,_st.y);
+    }
+    return _st;
+}
+
+void main() {
+    vec2 st = vTexCoord * float(30);
+    vec2 ipos = floor(st);  // integer
+    vec2 fpos = fract(st);  // fraction
+
+    vec2 tile = truchetPattern(fpos, random( ipos ));
+    float color = 0.0;
+
+    color = smoothstep(tile.x-0.3,tile.x,tile.y)-
+            smoothstep(tile.x,tile.x+0.3,tile.y);
+
+    gl_FragColor = vec4(vec3(color),1.0);
+}
+`
+export const mosaicMazeShader = {
+  vs: basicMosaicVS,
+  fs: mosaicMazeFS,
+  buffers: {
+    position: { type: vec4, n: 2 },
+    texCoord: { type: vec4, n: 2 },
+  },
+  uniforms: {
+    transMat: { type: mat4 },
+    rotateMat: { type: mat4 },
+    scaleMat: { type: mat4 },
+    projectionMat: { type: mat4 },
+    layer: { type: float },
+  },
+}
