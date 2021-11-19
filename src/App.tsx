11c1c55403488f21d08956988a8a38bc8334195e
blurname <naughtybao@outlook.com>
2021-11-19 22:51:02 +0800

    refactor: use react-router v6

--- src/App.tsx
+++ src/App.tsx
@@ -1,31 +1,36 @@
 import React, { useEffect, useRef, useState } from 'react'
-import { BrowserRouter as Router, Switch, Route, Link } from 'react-router-dom'
+import { BrowserRouter as Router, Route } from 'react-router-dom'
 import './App.css'
 import layer, { Content, Footer, Header } from 'antd/lib/layout/layout'
 import { Boxes } from './layout/Boxes'
 import { Layout } from './layout'
 import { Signin } from './layout/Sign/Signin'
 import { Signup } from './layout/Sign/Signup'
+import { Center } from './layout/User/Center'
+import { Head } from './layout/Head'
 function App() {
   return (
     <div className="App h-full">
       <Header className="bg-green-100">
-        <h1 className="text-blue-gray-900 font-large">picture editor</h1>
+        {/* <h1 className="text-blue-gray-900 font-large">picture editor</h1> */}
+        {/* <Head/> */}
       </Header>
       <div className="h-full">
         <Router>
-          <Route exact path="/canvas/:id">
-            <Layout />
+          {/* <Route  path="/canvas/:id"
+            element ={<Layout />}/>
+          <Route  path="/boxes"
+            element = {<Boxes />}/>*/}
+          {/* <Route  path="signin" element={<Signin />}/> */}
+          <Route  path="/signin">
+          <Signin />
           </Route>
-          <Route exact path="/boxes">
-            <Boxes />
-          </Route>
-          <Route exact path="/signin">
-            <Signin />
-          </Route>
-          <Route exact path="/signup">
-            <Signup />
-          </Route>
+          <Route  path="/signup"
+            >
+              <Signup/>
+              </Route>
+          {/* <Route  path="/usercenter"
+            element = {<Center />}/> */}
         </Router>
       </div>
       <Footer className="bg-green-200" />
